46ec58a50d6a5195dfb1758cb5c43d365187df96
Karl Broman <kbroman@gmail.com>
2024-04-30 16:14:55 -0500

    Add a tiny bit of code

---
+++ R/find_rpeaks_sliding.R
@@ -0,0 +1,29 @@
+#' Detect R peaks in a raw ECG signal, using a sliding window
+#'
+#' Detect R peaks in a raw ECG signal, using [rsleep::detect_rpeaks()] but using a sliding window.
+#'
+#' @param signal Numerical vector of ECG signal
+#'
+#' @param window Integer indicating the number of values to consider at one time
+#'
+#' @param sRate ECG signal rate
+#'
+#' @param ... Passed to [rsleep::detect_rpeaks()]
+#'
+#' @param return_index If TRUE, the index for each R peak is returned instead of the timing
+#'
+#' @param adjust If TRUE, adjust the results to hit the nearby local maxima
+#'
+#' @importFrom rsleep detect_rpeaks
+#'
+#'
+#' @export
+
+find_rpeaks_sliding <-
+    function(signal, window=2.5e5, sRate=1e9/7682304,
+             ..., return_index=TRUE, adjust=TRUE)
+{
+    peaks <- rsleep::detect_rpeaks(signal, sRate=sRate, ..., return_index=return_index)
+
+    peaks
+}
