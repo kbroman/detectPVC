7c46fdcdd5293740903101cd497b9b318f130b12
Karl Broman <kbroman@gmail.com>
2024-04-30 21:04:27 -0500

    Fix tests because I'd added peak adjustment

--- tests/testthat/test-detect_rpeaks_sliding.R
+++ tests/testthat/test-detect_rpeaks_sliding.R
@@ -4,17 +4,26 @@ test_that("simple test of detect_rpeaks_sliding", {
 
     data(h10)
 
-    expect_equal(detect_rpeaks_sliding(h10$ecg),
+    # don't adjust peak positions
+    expect_equal(detect_rpeaks_sliding(h10$ecg, adjust=FALSE),
                  rsleep::detect_rpeaks(h10$ecg, sRate=1e9/7682304, return_index=TRUE))
 
+    expect_equal(detect_rpeaks_sliding(h10$ecg, window=2000, adjust=FALSE),
+                 detect_rpeaks_sliding(h10$ecg, adjust=FALSE))
+
+    # don't adjust peak positions; return times not indices
+    expect_equal(detect_rpeaks_sliding(h10$ecg, return_index=FALSE, adjust=FALSE),
+                 rsleep::detect_rpeaks(h10$ecg, sRate=1e9/7682304, return_index=FALSE))
+
+    expect_equal(detect_rpeaks_sliding(h10$ecg, window=2000, return_index=FALSE, adjust=FALSE),
+                 detect_rpeaks_sliding(h10$ecg, return_index=FALSE, adjust=FALSE))
+
+
+    # adjust peak positions
     expect_equal(detect_rpeaks_sliding(h10$ecg, window=2000),
                  detect_rpeaks_sliding(h10$ecg))
 
-    expect_equal(detect_rpeaks_sliding(h10$ecg, return_index=FALSE),
-                 rsleep::detect_rpeaks(h10$ecg, sRate=1e9/7682304, return_index=FALSE))
-
     expect_equal(detect_rpeaks_sliding(h10$ecg, window=2000, return_index=FALSE),
                  detect_rpeaks_sliding(h10$ecg, return_index=FALSE))
-
 
 })
