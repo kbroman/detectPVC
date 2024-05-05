438edd13e0741fc65c731bb0f70cd8c790316226
Karl Broman <kbroman@gmail.com>
2024-05-05 17:32:04 -0500

    plot.pvc_stats: add ylab and ylim args for each plot

--- R/plot_pvc_stats.R
+++ R/plot_pvc_stats.R
@@ -2,9 +2,19 @@
 #'
 #' Plot running PVC statistics from [running_pvc_stats()]
 #'
-#' @param pvc_stats Data frame of running PVC stats from [running_pvc_stats()]
+#' @param x Data frame of running PVC stats from [running_pvc_stats()]
+#'
+#' @param ylab_pvc Y-axis label for the plot of percent PVC
+#'
+#' @param ylab_hr  Y-axis label for the plot of heart rate
+#'
+#' @param ylim_pvc Y-axis limits for the plot of percent PVC
+#'
+#' @param ylim_hr Y-axis limits for the plot of heart rate
 #'
 #' @param ... Passed to [broman::grayplot()]
+#'
+#' @details Makes a plot with two panels, of percent PVC vs time and heart rate vs time.
 #'
 #' @return None.
 #'
@@ -22,19 +32,19 @@
 #' plot(pvc_stats)
 
 plot.pvc_stats <-
-    function(x, ...)
+    function(x,
+             ylab_pvc="Percent PVC", ylab_hr="Heart rate (BPM)",
+             ylim_pvc=c(0, max(x$pvc)*1.02), ylim_hr=c(range(x$hr))*c(0.98, 1.02),
+             ...)
 {
     old_mfrow <- par("mfrow")
     old_mar <- par("mar")
     on.exit(par(mfrow=old_mfrow, mar=old_mar))
 
     internal_plot <-
-        function(x, ..., xlab="Time", ylab1="Percent PVC",
-                 ylab2="Heart rate (BPM)", type="l",
+        function(x, ..., xlab="Time", type="l",
                  mgp=c(2.1, 0.5, 0), mgp.x=NULL, mgp.y=NULL,
                  mar=c(3.1, 3.6, 1.6, 0.6), xaxs="i", yaxs="i",
-                 ylim1=c(0, max(x$pvc)*1.02),
-                 ylim2=c(range(x$hr))*c(0.98, 1.02),
                  col="darkslateblue", lwd=2)
     {
         par(mar=mar)
@@ -44,12 +54,12 @@ plot.pvc_stats <-
         if(is.null(mgp.y)) mgp.y <- mgp
 
         par(mfrow=c(2,1))
-        grayplot(x$time, x$pvc, type=type, ..., xat=NA, ylab=ylab1, xlab=xlab,
-                 xaxs=xaxs, yaxs=yaxs, ylim=ylim1, col=col, lwd=lwd)
+        grayplot(x$time, x$pvc, type=type, ..., xat=NA, ylab=ylab_pvc, xlab=xlab,
+                 xaxs=xaxs, yaxs=yaxs, ylim=ylim_pvc, col=col, lwd=lwd)
         axis(side=1, mgp=mgp.x, at=xax$x, label=xax$label, tick=FALSE)
 
-        grayplot(x$time, x$hr, type=type, ..., xat=NA, ylab=ylab2, xlab=xlab,
-                 xaxs=xaxs, yaxs=yaxs, ylim=ylim2, col=col, lwd=lwd)
+        grayplot(x$time, x$hr, type=type, ..., xat=NA, ylab=ylab_hr, xlab=xlab,
+                 xaxs=xaxs, yaxs=yaxs, ylim=ylim_hr, col=col, lwd=lwd)
         axis(side=1, mgp=mgp.x, at=xax$x, label=xax$label, tick=FALSE)
     }
 
