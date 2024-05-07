848b85ac52d4f92281a5e434847975d7caa39a9f
Karl Broman <kbroman@gmail.com>
2024-05-07 09:25:33 -0500

    read_multcsv: add args dont_modify and tz; default is to reorder rows and add datetime column

--- R/read_multcsv.R
+++ R/read_multcsv.R
@@ -7,8 +7,19 @@
 #' @param files Optional character vector of file names. If not provided, we use
 #'     `list.files(dir)` to grab all CSV files in the directory `dir`.
 #'
+#' @param dont_modify If TRUE, just rbind the file contents together. If FALSE,
+#' look for a `time` column and reorder the rows by that column, and then
+#' add a converted `datetime` column using [convert_timestamp()].
+
+#' @param tz Time zone, used if `dont_modify=FALSE` and there is a time
+#' column to convert.
+#'
 #' @details At least one of `files` or `dir` must be provided.
 #' The files should all have the same set of columns.
+#'
+#' If `dont_modify=FALSE` and there is a column `time`, the rows are
+#' reordered using this column and a `datetime` column is added,
+#' converting `time` with [convert_timestamp()]
 #'
 #' @return A data.frame with the contents of all files row-binded together.
 #'
@@ -16,7 +27,7 @@
 #' @export
 
 read_multcsv <-
-    function(dir=".", files=NULL)
+    function(dir=".", files=NULL, dont_modify=FALSE, tz=Sys.timezone())
 {
     if(is.null(files)) {
         if(is.null(dir)) stop("Provide at least one of files or dir")
@@ -29,5 +40,12 @@ read_multcsv <-
     }
     if(!is.null(dir)) files <- file.path(dir, files)
 
-    do.call("rbind", lapply(files, read.csv))
+    result <- do.call("rbind", lapply(files, read.csv))
+
+    if(!dont_modify && "time" %in% colnames(result)) {
+        result <- result[order(result$time), , drop=FALSE]
+        result$datetime <- convert_timestamp(result$time, tz=tz)
+    }
+
+    result
 }
