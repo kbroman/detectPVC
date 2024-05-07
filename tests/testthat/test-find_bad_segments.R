239790fd98ad00b8ae487378a27524cba8d5c2eb
Karl Broman <kbroman@gmail.com>
2024-05-07 08:18:30 -0500

    Add test of find_bad_segments w/ return_index=FALSE

--- tests/testthat/test-find_bad_segments.R
+++ tests/testthat/test-find_bad_segments.R
@@ -3,6 +3,7 @@ context("find_bad_segments")
 test_that("simple test of find_bad_segments", {
 
     data(h10)
+    h10$datetime <- convert_timestamp(h10$time, "America/Chicago")
 
     expect_equal(find_bad_segments(h10$time, h10$ecg),
                  structure(list(start = numeric(0), end = numeric(0)),
@@ -11,6 +12,10 @@ test_that("simple test of find_bad_segments", {
     h10$ecg[3896:3931] <- 2.5
     expect_equal(find_bad_segments(h10$time, h10$ecg),
                  structure(list(start = 3490, end = 4339),
+                           class = "data.frame", row.names = c(NA, -1L)))
+
+    expect_equal(find_bad_segments(h10$time, h10$ecg, return_index=FALSE),
+                 structure(list(start = h10$datetime[3490], end = h10$datetime[4339]),
                            class = "data.frame", row.names = c(NA, -1L)))
 
 })
