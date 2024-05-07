8694ffd086eb314a758f15702a67838016a234fc
Karl Broman <kbroman@gmail.com>
2024-05-06 23:03:13 -0500

    find_bad_segments: also look for regions with missing data
      - further, merge adjacent intervals if overlapping
    
      - split off a couple of bits of code as separate internal functions

--- R/find_bad_segments.R
+++ R/find_bad_segments.R
@@ -1,6 +1,7 @@
 #' Find bad segments in ECG signal
 #'
-#' Find particularly noisy regions in ECG signal, with large values far from 0
+#' Find particularly noisy regions in ECG signal, with large values
+#' far from 0, plus also regions with excessive missing data.
 #'
 #' @param time Vector of times (integers as from Polar H10, datetimes, or character strings)
 #'
@@ -9,6 +10,8 @@
 #' @param absval_thresh Threshold on absolute value of signal
 #'
 #' @param runmean_thresh Threshold on running mean of absolute value of signal
+#'
+#' @param missing_thresh Maximum amount of missing data within 1 second
 #'
 #' @param window Window for running mean (in seconds)
 #'
@@ -33,20 +36,46 @@
 
 find_bad_segments <-
     function(time, signal, absval_thresh=2, runmean_thresh=0.7,
-             window=0.2, min_gap=2000, pad=400, tz=Sys.timezone())
+             missing_thresh=70, window=0.2, min_gap=2000, pad=400,
+             tz=Sys.timezone())
 {
     stopifnot(length(time) == length(signal))
 
     time <- convert_timestamp(time, tz=tz)
 
+    # smooth absolute signal
     running_mean <- broman::runningmean(time, abs(signal), time, window=0.2)
 
+    # regions with large values
     w <- which(abs(signal) > absval_thresh | running_mean > runmean_thresh)
 
     if(length(w)==0) {
-        return(data.frame(start=numeric(0), end=numeric(0)))
+        badsegs <- data.frame(start=numeric(0), end=numeric(0))
     }
+    else badsegs <- vector_to_segments(w, min_gap)
 
+    # look for regions with lots of missing data
+    # count data points every sec
+    n_data <- broman::runningmean(time, rep(1, length(time)), at=time, window=1, "sum")
+    w <- which(n_data < 130-missing_thresh)
+    if(length(w)>0) badsegs <- rbind(badsegs, vector_to_segments(w, min_gap))
+    if(nrow(badsegs)==0) return(badsegs)
+
+    if(nrow(badsegs) > 1) badsegs <- badsegs[order(badsegs[,1], badsegs[,2]),]
+
+    # pad the bad segments on each end
+    badsegs[,1] <- badsegs[,1] - pad
+    badsegs[,2] <- badsegs[,2] + pad
+    badsegs[badsegs < 1] <- 1
+    badsegs[badsegs > length(time)] <- length(time)
+
+    as.data.frame(badsegs)
+}
+
+
+vector_to_segments <-
+    function(w, min_gap=2000)
+{
     dw <- diff(w)
     br <- which(dw > min_gap)
 
@@ -56,12 +85,23 @@ find_bad_segments <-
     badsegs[nrow(badsegs),2] <- max(w)
 
     colnames(badsegs) <- c("start", "end")
+    badsegs
+}
 
-    # pad the bad segments on each end
-    badsegs[,1] <- badsegs[,1] - pad
-    badsegs[,2] <- badsegs[,2] + pad
-    badsegs[badsegs < 1] <- 1
-    badsegs[badsegs > length(time)] <- length(time)
+merge_overlaps <-
+    function(badsegs)
+{
+    # merge overlap
+    d <- badsegs[-nrow(badsegs),2] - badsegs[-1,1]
+    while(nrow(badsegs) > 1 && any(d > 0)) {
+        omit <- NULL
+        for(i in which(d > 0)) {
+            badsegs[i+1,1] <- badsegs[i,1]
+            omit <- c(omit, i)
+        }
+        badsegs <- badsegs[-omit,,drop=FALSE]
+        d <- badsegs[-nrow(badsegs),2] - badsegs[-1,1]
+    }
 
-    as.data.frame(badsegs)
+    badsegs
 }
